fb1bac7da5e08d6abdf9b4c04d2dfba252b2fc14
MahmoudMetwalli <mammkbih@gmail.com>
2024-03-27 17:23:24 +0200

    2024-03-27 05:23:24 PM

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -57,6 +57,8 @@ int binary_tree_is_full(const binary_tree_t *tree)
 */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 	if (binary_tree_balance(tree) == 0 && binary_tree_is_full(tree) == 1)
 		return (1);
 	else
