184f412a94f35e1f79f897c6d8690075737be16c
RamzyAR7 <rameya683@gmail.com>
2024-03-27 22:26:31 +0200

    add v0 -> v11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,7 +6,15 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t len = 0;
+
 	if (tree == NULL)
+	{
 		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	}
+
+	len = 1 + binary_tree_size(tree->left);
+	len += binary_tree_size(tree->right);
+
+	return (len);
 }
