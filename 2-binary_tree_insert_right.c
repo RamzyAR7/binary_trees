7617ceaf342283dc3d20b73d4954c149d736fb19
RamzyAR7 <rameya683@gmail.com>
2024-03-27 15:39:37 +0200

    add v0 -> v8

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = malloc(sizeof(binary_tree_t));
+
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+
+	if (parent->right == NULL)
+	{
+		parent->right = node;
+		node->right = NULL;
+	}
+	else
+	{
+		node->right = parent->right;
+		parent->right->parent = node;
+		parent->right = node;
+	}
+
+	return (node);
+}
