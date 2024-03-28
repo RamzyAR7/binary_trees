38e7a8c37cddba0fb901fcd4b048b881f5158577
RamzyAR7 <rameya683@gmail.com>
2024-03-29 01:45:18 +0200

    add readme

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,7 +6,7 @@
  * @tree: pointer to the root node of the tree to traverse
  * @func: pointer to a function to call for each node
  * Return: void
- * by ramzy
+ * by ahmed ramzy
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
